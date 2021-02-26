1ea0a806179d64bf09d4b24d9c00ed9b7ceaedb6
ruanzhihao <ruanzhihao99@163.com>
2021-02-26 11:36:31 +0800

    第二次提交

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Hello {
     //入口主函数
     public static void main(String[] args) {
-     
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请输入GitHub账号：");
+        String str=scanner.nextLine();
+        System.out.println("请输入GitHub密码：");
+        String str1=scanner.nextLine();
     }
 }
